669d8c5de7320bb877ba598f12add81943f84f0b
gonefabi <122861858+gonefabi@users.noreply.github.com>
2023-12-10 15:32:30 +0100

    Systemdatum ziehen und anzeigen lassen

--- src/main/java/com/jkfd/oopii/Controller/HomeController.java
+++ src/main/java/com/jkfd/oopii/Controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.jkfd.oopii.Controller;
 
+
 import com.jkfd.oopii.Date;
 import com.jkfd.oopii.HelloApplication;
 import javafx.fxml.FXML;
@@ -7,9 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,10 +18,19 @@ import java.util.ResourceBundle;
  * @Author: Fabian Klein
  * @Version: 0.1
  */
-public class HomeController  {
+public class HomeController implements Initializable{
+    @FXML
+    Label CurrentDate;
 
+    Date currentDate = new Date();
+
+    /**
+     * Initializes the home-view
+     * @param stage
+     * @throws IOException
+     */
     public static void loadView(Stage stage) throws IOException{
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/jkfd/oopii/home-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HomeController.class.getResource("/com/jkfd/oopii/home-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Home");
         stage.setScene(scene);
@@ -30,5 +38,22 @@ public class HomeController  {
 
     }
 
+    public void displayCurrentDate()
+    {
+        CurrentDate.setText(currentDate.getCurrentDate());
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        displayCurrentDate();
+    }
+
+
+
+
 
 }
+
+
+
