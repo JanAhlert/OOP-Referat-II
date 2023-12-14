0e7a523ca9d52bbce141741faa606f1f112e3684
Lucerado <ienchi.kevin@icloud.com>
2023-12-14 21:16:25 +0100

    New Design but not ready yet Need the Event list and to do list to build in

--- src/main/java/com/jkfd/oopii/HelloApplication.java
+++ src/main/java/com/jkfd/oopii/HelloApplication.java
@@ -3,6 +3,7 @@ package com.jkfd.oopii;
 
 
 import com.jkfd.oopii.CalenderView.CalenderView;
+import com.jkfd.oopii.Controller.HomeController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import java.io.IOException;
@@ -12,7 +13,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-
+        HomeController.loadView(stage);
     }
 
     public static void main(String[] args) {
