acc4f70816b6c3fef99363d7cd2e4197405a43ba
Lucerado <67416003+Lucerado@users.noreply.github.com>
2023-12-15 22:08:41 +0100

    Abstracte Classe für einen Abstracten Controller hinzugefügt

--- src/main/java/com/jkfd/oopii/Abstract/AbstractController.java
+++ src/main/java/com/jkfd/oopii/Abstract/AbstractController.java
@@ -1,6 +1,17 @@
 package com.jkfd.oopii.Abstract;
 
+import java.util.ArrayList;
+
 public abstract class AbstractController {
 
 
+
+
+    public void load(int vController)
+    {
+
+
+
+
+    }
 }
