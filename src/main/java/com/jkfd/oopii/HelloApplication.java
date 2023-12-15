d0e01b53c64a7072f203f7c6678706a2d8017f49
Lucerado <67416003+Lucerado@users.noreply.github.com>
2023-12-15 21:04:26 +0100

    -Design Anpassungen an month-view.fxml

--- src/main/java/com/jkfd/oopii/HelloApplication.java
+++ src/main/java/com/jkfd/oopii/HelloApplication.java
@@ -2,6 +2,7 @@ package com.jkfd.oopii;
 
 
 
+import com.jkfd.oopii.Controller.MonthViewController;
 import com.jkfd.oopii.Controller.YearViewController;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -12,7 +13,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-       YearViewController.loadView(stage);
+       MonthViewController.loadView(stage);
     }
 
     public static void main(String[] args) {
