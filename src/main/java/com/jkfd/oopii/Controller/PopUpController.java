85a7fd738ec2ed322a8bf1e6cfdd7847004e99de
Lucerado <67416003+Lucerado@users.noreply.github.com>
2024-01-11 16:32:30 +0100

    All relevant elements from the file PopUpEdit got an fxid for coding purpose

--- src/main/java/com/jkfd/oopii/Controller/PopUpController.java
+++ src/main/java/com/jkfd/oopii/Controller/PopUpController.java
@@ -1,26 +1,40 @@
 package com.jkfd.oopii.Controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class PopUpController {
 
-    private static FXMLLoader fxmlLoader = new FXMLLoader(PopUpController.class.getResource("/com/jkfd/oopii/PopUpEdit.fxml"));    //FXMLLoader for the pop-upView
+    private static FXMLLoader fxmlLoader_edit = new FXMLLoader(PopUpController.class.getResource("/com/jkfd/oopii/PopUpEdit.fxml"));    //FXMLLoader for the pop-upView
+    private static FXMLLoader fxmlLoader_check = new FXMLLoader(PopUpController.class.getResource(("/com/jkfd/oopii/PopUpCheck.fxml")));
+
+    private static FXMLLoader fxmlLoader_savedmessage = new FXMLLoader(PopUpController.class.getResource("/com/jkfd/oopii/PopUpSavedMessage.fxml"));
+
+    private static FXMLLoader fxmlLoader_warningmessage = new FXMLLoader(PopUpController.class.getResource("/com/jkfd/oopii/PopUpWarningMessage.fxml"));
+
 
     /**
      * Method to load the pop-upView file and set the scene in the stage
      * @param stage
      * @throws IOException
      */
+
+
     public static void loadView(Stage stage) throws IOException {
-            Scene scene = new Scene(fxmlLoader.load());
+            Scene scene = new Scene(fxmlLoader_edit.load());
             stage.setScene(scene);
             stage.show();
             stage.setTitle("Termin bearbeiten");
     }
+
+
+
+
 }
