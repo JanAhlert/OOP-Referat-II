52eba660bd216ef06bed8b5f87a6b2bedddf4ab3
Jan Ahlert <jan.ahlert@iu-study.org>
2024-01-11 17:09:13 +0100

    Subsystems 2

--- src/main/java/com/jkfd/oopii/Subsystem/Subsystems/EventReminderSubsystem.java
+++ src/main/java/com/jkfd/oopii/Subsystem/Subsystems/EventReminderSubsystem.java
@@ -1,7 +1,12 @@
 package com.jkfd.oopii.Subsystem.Subsystems;
 
+import com.jkfd.oopii.Database.Models.Event;
 import com.jkfd.oopii.Subsystem.Subsystem;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+
+import static com.jkfd.oopii.HelloApplication.databaseManager;
 
 public class EventReminderSubsystem extends Subsystem {
     public EventReminderSubsystem() {
@@ -11,6 +16,10 @@ public class EventReminderSubsystem extends Subsystem {
     @Override
     public void Fire() {
         try {
+            ArrayList<Event> events = databaseManager.GetEvents();
+
+            // TODO: Check if event is happening soon
+
             LoggerFactory.getLogger(EventReminderSubsystem.class).info(this.name + " subsystem fired.");
         } catch(Exception e) {
             super.HandleException(e);
