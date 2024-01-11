9e2ea7b6905ec7781252ffac47dd7ce087afddbf
Jan Ahlert <jan.ahlert@iu-study.org>
2024-01-11 11:00:43 +0100

    Unfinished Todos

--- src/main/java/com/jkfd/oopii/Database/Models/Todo.java
+++ src/main/java/com/jkfd/oopii/Database/Models/Todo.java
@@ -1,4 +1,25 @@
 package com.jkfd.oopii.Database.Models;
 
+import java.sql.Date;
+
 public class Todo extends Element {
+    /**
+     * The date(time) when the todo was completed.
+     */
+    private Date completedDate;
+
+    /**
+     * Sets the todo completed at the specific date that was supplied.
+     * Set this to null to set as uncompleted.
+     * @param date completed date
+     */
+    public void SetCompletedDate(Date date)
+    {
+        this.completedDate = date;
+    }
+
+    public Date GetCompletedDate()
+    {
+        return this.completedDate;
+    }
 }
