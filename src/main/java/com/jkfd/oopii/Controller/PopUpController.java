aae39f50d6f982c878397db1893ef96f82795fb8
Lucerado <67416003+Lucerado@users.noreply.github.com>
2024-01-11 20:35:39 +0100

    Errors resolved. XAML and Java Class coneccted.

--- src/main/java/com/jkfd/oopii/Controller/PopUpController.java
+++ src/main/java/com/jkfd/oopii/Controller/PopUpController.java
@@ -25,51 +25,80 @@ public class PopUpController {
      * @throws IOException
      */
 
-    @FXML
+
 
     //The Elements are from the File PopUpEdit.fxml --> The following Elements are from the Tab ToDo
-
+    @FXML
     private TextField PopUpEdit_ToDo_TextField;
+    @FXML
     private TextArea PopUpEdit_ToDo_TextArea;
+    @FXML
     private ChoiceBox PopUpEdit_ToDo_PriorityChoiceBox;
+    @FXML
     private DatePicker PopUpEdit_ToDo_DatePicker;
+    @FXML
     private Button PopUpEdit_ToDo_SaveButton;
+    @FXML
     private Button PopUpEdit_ToDo_CancelButton;
+    @FXML
     private Button PopUpEdit_ToDo_CloseButton;
 
     //The Elements are from the File PopUpEdit.fxml --> The following Elements are from the Tab Events
 
+    @FXML
     private TextField PopUpEdit_Event_TextField;
+    @FXML
     private DatePicker PopUpEdit_Event_DatePicker;
+    @FXML
     private DatePicker PopUpEdit_Event_TimePicker;
+    @FXML
     private TextArea PopUpEdit_Event_TextArea;
 
+    @FXML
     private ChoiceBox PopUpEdit_Event_PriorityChoiceBox;
+    @FXML
     private Button PopUpEdit_Event_SaveButton;
+    @FXML
     private Button PopUpEdit_Event_CancelButton;
+    @FXML
     private Button PopUpEdit_Event_CloseButton;
 
     //The Elements are from the File PopUpCheck --> The following Elements are from the Tab "Aktuelle Aufgaben und Events".
 
+    @FXML
     private Button PopUpCheck_ActiveEventsToDos_activToD_editButton;
+    @FXML
     private Button PopUpCheck_ActiveEventsToDos_activToD_deleteButton;
+    @FXML
     private Button PopUpCheck_ActiveEventsToDos_activEvent_editButton;
+    @FXML
     private Button PopUpCheck_ActiveEventsToDos_activEvent_deleteButton;
+    @FXML
     private Label  PopUpCheck_ActiveEventsToDos_LabelMessage;
+    @FXML
     private Button PopUpCheck_ActiveEventsToDos_SaveButton;
+    @FXML
     private Button PopUpCheck_ActiveEventsToDos_CancelButton;
+    @FXML
     private Button PopUpCheck_ActiveEventsToDos_CloseButton;
 
     //The Elements are from the File PopUpCheck --> The following Elements are from the Tab "Erledigte Aufgaben und Events".
 
+    @FXML
     private Button PopUpCheck_finishedEventsToDos_delTodos_RecoveryButton;
+    @FXML
     private Button PopUpCheck_finishedEventsToDos_delTodos_FinallydelButton;
+    @FXML
     private Button PopUpCheck_finishedEventsToDos_delEvents_RecoveryButton;
+    @FXML
     private Button PopUpCheck_finishedEventsToDos_delEvents_FinallyButton;
-
+    @FXML
     private Label PopUpCheck_finishedEventsToDos_delEvents_LabelMessage;
+    @FXML
     private Button PopUpCheck_finishedEventsToDos_SaveButton;
+    @FXML
     private Button PopUpCheck_finishedEventsToDos_CancelButton;
+    @FXML
     private Button PopUpCheck_finishedEventsToDos_CloseButton;
 
 
