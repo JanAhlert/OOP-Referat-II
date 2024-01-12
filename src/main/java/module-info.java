16703db472e4b7e5f0e789f6b84884fb792c0288
Jan Ahlert <128168265+JanAhlert@users.noreply.github.com>
2024-01-12 12:42:02 +0100

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,6 @@ module com.jkfd.oopii {
     requires org.controlsfx.controls;
     requires java.sql;
     requires com.calendarfx.view;
-    requires org.slf4j;
 
     opens com.jkfd.oopii to javafx.fxml;
     exports com.jkfd.oopii;
