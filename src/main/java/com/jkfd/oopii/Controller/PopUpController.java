95b365fd437062de199801bd4cfd5e88aab92c20
Jan Ahlert <jan.ahlert@iu-study.org>
2024-01-12 22:08:01 +0100

    More event loading stuff

--- src/main/java/com/jkfd/oopii/Controller/PopUpController.java
+++ src/main/java/com/jkfd/oopii/Controller/PopUpController.java
@@ -1,15 +1,26 @@
 package com.jkfd.oopii.Controller;
 
+import com.jkfd.oopii.Database.Models.Element;
+import com.jkfd.oopii.Database.Models.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.time.ZoneId;
+
+import static com.jkfd.oopii.HelloApplication.databaseManager;
 
 public class PopUpController {
+    final Logger logger = LoggerFactory.getLogger(MonthViewController.class);
 
     private static FXMLLoader fxmlLoader_edit = new FXMLLoader(PopUpController.class.getResource("/com/jkfd/oopii/PopUpEdit.fxml"));    //FXMLLoader for the pop-upView
     private static FXMLLoader fxmlLoader_check = new FXMLLoader(PopUpController.class.getResource(("/com/jkfd/oopii/PopUpCheck.fxml")));
@@ -17,14 +28,6 @@ public class PopUpController {
     private static FXMLLoader fxmlLoader_savedmessage = new FXMLLoader(PopUpController.class.getResource("/com/jkfd/oopii/PopUpSavedMessage.fxml"));
 
     private static FXMLLoader fxmlLoader_warningmessage = new FXMLLoader(PopUpController.class.getResource("/com/jkfd/oopii/PopUpWarningMessage.fxml"));
-
-
-    /**
-     * Method to load the pop-upView file and set the scene in the stage
-     * @param stage
-     * @throws IOException
-     */
-
 
 
     //The Elements are from the File PopUpEdit.fxml --> The following Elements are from the Tab ToDo
@@ -101,15 +104,37 @@ public class PopUpController {
     @FXML
     Button PopUpCheck_finished_CloseButton;
 
+    /**
+     * Method to load the pop-upView file and set the scene in the stage
+     * @param stage
+     * @throws IOException
+     */
     public static void loadView(Stage stage) throws IOException {
-            Scene scene = new Scene(fxmlLoader_edit.load());
-            stage.setScene(scene);
-            stage.show();
-            stage.setTitle("Termin bearbeiten");
+        Scene scene = new Scene(fxmlLoader_edit.load());
+        stage.setScene(scene);
+        stage.show();
+        stage.setTitle("Termin bearbeiten");
     }
 
+    @FXML
+    private void onPopUpEditEventSaveClicked(){
+        try {
+            Event tmp = new Event();
+            tmp.SetID(100);
+            tmp.title = PopUpEdit_Event_TextField.getText();
+            tmp.description = PopUpEdit_Event_TextArea.getText();
+            tmp.priority = Element.Priority.NORMAL; // TODO: Get from dropdown
+            tmp.SetDateRange(java.sql.Date.valueOf(PopUpEdit_Event_DatePicker.getValue()), java.sql.Date.valueOf(PopUpEdit_Event_DatePicker.getValue())); // TODO: Proper Start/End Date
 
+            databaseManager.CreateEvent(tmp);
+        } catch (Exception e) {
+            logger.atError().setMessage("onPopUpEditEventSaveClicked error: {}").addArgument(e.getMessage()).log();
 
-
-
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Fehler");
+            alert.setHeaderText("Ein Fehler ist aufgetreten");
+            alert.setContentText("Es gab ein Problem beim speichern des Events.\n" + e.getLocalizedMessage());
+            alert.showAndWait();
+        }
+    }
 }
