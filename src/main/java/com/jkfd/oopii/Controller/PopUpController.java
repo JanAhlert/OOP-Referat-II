6086d002d784335a923a221d470990a09a5099e0
Jan Ahlert <jan.ahlert@iu-study.org>
2024-01-13 23:19:53 +0100

    More working ui

--- src/main/java/com/jkfd/oopii/Controller/PopUpController.java
+++ src/main/java/com/jkfd/oopii/Controller/PopUpController.java
@@ -2,21 +2,18 @@ package com.jkfd.oopii.Controller;
 
 import com.jkfd.oopii.Database.Models.Element;
 import com.jkfd.oopii.Database.Models.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.sql.Date;
-import java.time.ZoneId;
 
+import static com.jkfd.oopii.Controller.MonthViewController.observedEvent;
+import static com.jkfd.oopii.Controller.MonthViewController.observedEventAction;
 import static com.jkfd.oopii.HelloApplication.databaseManager;
 
 public class PopUpController {
@@ -24,9 +21,6 @@ public class PopUpController {
 
     private static FXMLLoader fxmlLoader_edit = new FXMLLoader(PopUpController.class.getResource("/com/jkfd/oopii/PopUpEdit.fxml"));    //FXMLLoader for the pop-upView
     private static FXMLLoader fxmlLoader_check = new FXMLLoader(PopUpController.class.getResource(("/com/jkfd/oopii/PopUpCheck.fxml")));
-
-    private static FXMLLoader fxmlLoader_savedmessage = new FXMLLoader(PopUpController.class.getResource("/com/jkfd/oopii/PopUpSavedMessage.fxml"));
-
     private static FXMLLoader fxmlLoader_warningmessage = new FXMLLoader(PopUpController.class.getResource("/com/jkfd/oopii/PopUpWarningMessage.fxml"));
 
 
@@ -41,33 +35,22 @@ public class PopUpController {
     DatePicker PopUpEdit_ToDo_DatePicker;
     @FXML
     Button PopUpEdit_ToDo_SaveButton;
-    @FXML
-    Button PopUpEdit_ToDo_CancelButton;
-    @FXML
-    Button PopUpEdit_ToDo_CloseButton;
 
     //The Elements are from the File PopUpEdit.fxml --> The following Elements are from the Tab Events
-
     @FXML
-    TextField PopUpEdit_Event_TextField;
+    TextField PopUpEdit_Event_TitleTextField;
     @FXML
-    DatePicker PopUpEdit_Event_DatePicker;
+    DatePicker PopUpEdit_Event_StartDatePicker;
     @FXML
-    DatePicker PopUpEdit_Event_TimePicker;
+    DatePicker PopUpEdit_Event_EndDatePicker;
     @FXML
-    TextArea PopUpEdit_Event_TextArea;
-
+    TextArea PopUpEdit_Event_DescriptionTextArea;
     @FXML
     ChoiceBox PopUpEdit_Event_PriorityChoiceBox;
     @FXML
     Button PopUpEdit_Event_SaveButton;
-    @FXML
-    Button PopUpEdit_Event_CancelButton;
-    @FXML
-    Button PopUpEdit_Event_CloseButton;
 
     //The Elements are from the File PopUpCheck --> The following Elements are from the Tab "Aktuelle Aufgaben und Events".
-
     @FXML
     Button PopUpCheck_activToD_editButton;
     @FXML
@@ -86,7 +69,6 @@ public class PopUpController {
     Button PopUpCheck_ActiveEventsToDos_CloseButton;
 
     //The Elements are from the File PopUpCheck --> The following Elements are from the Tab "Erledigte Aufgaben und Events".
-
     @FXML
     Button PopUpCheck_finished_delTodos_RecoveryButton;
     @FXML
@@ -116,25 +98,62 @@ public class PopUpController {
         stage.setTitle("Termin bearbeiten");
     }
 
+    public void initialize()
+    {
+        // Fill the priority choice box with all priority possibilities.
+        PopUpEdit_Event_PriorityChoiceBox.getItems().setAll(Element.Priority.values());
+
+        if (observedEvent != null) {
+            PopUpEdit_Event_TitleTextField.setText(observedEvent.title);
+            PopUpEdit_Event_DescriptionTextArea.setText(observedEvent.description);
+            PopUpEdit_Event_StartDatePicker.setValue(observedEvent.GetStartDate().toLocalDate());
+            PopUpEdit_Event_EndDatePicker.setValue(observedEvent.GetEndDate().toLocalDate());
+
+            PopUpEdit_Event_PriorityChoiceBox.setValue(observedEvent.priority);
+        } else {
+            PopUpEdit_Event_PriorityChoiceBox.setValue(Element.Priority.NORMAL);
+        }
+    }
+
     @FXML
     private void onPopUpEditEventSaveClicked(){
         try {
             Event tmp = new Event();
-            tmp.SetID(100);
-            tmp.title = PopUpEdit_Event_TextField.getText();
-            tmp.description = PopUpEdit_Event_TextArea.getText();
-            tmp.priority = Element.Priority.NORMAL; // TODO: Get from dropdown
-            tmp.SetDateRange(java.sql.Date.valueOf(PopUpEdit_Event_DatePicker.getValue()), java.sql.Date.valueOf(PopUpEdit_Event_DatePicker.getValue())); // TODO: Proper Start/End Date
+            tmp.title = PopUpEdit_Event_TitleTextField.getText();
+            tmp.description = PopUpEdit_Event_DescriptionTextArea.getText();
+            tmp.priority = (Element.Priority) PopUpEdit_Event_PriorityChoiceBox.getValue();
+            tmp.SetDateRange(java.sql.Date.valueOf(PopUpEdit_Event_StartDatePicker.getValue()), java.sql.Date.valueOf(PopUpEdit_Event_EndDatePicker.getValue()));
 
-            databaseManager.CreateEvent(tmp);
+            if (observedEventAction == 0) {
+                databaseManager.CreateEvent(tmp);
+            } else if (observedEventAction == 1) {
+                tmp.SetID(observedEvent.GetID());
+                databaseManager.UpdateEvent(tmp);
+            } else {
+                logger.atError().setMessage("Invalid observedEventAction: {}").addArgument(observedEventAction).log();
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Fehler");
+                alert.setHeaderText("Ein Fehler ist aufgetreten");
+                alert.setContentText("Wert von observedEventAction unbekannt: " + observedEventAction);
+                alert.showAndWait();
+            }
+
+            Alert info = new Alert(Alert.AlertType.INFORMATION);
+            info.setTitle("");
+            info.setHeaderText("Hinweis!");
+            info.setContentText("Ihre änderungen wurden gespeichert!");
+            info.showAndWait();
         } catch (Exception e) {
-            logger.atError().setMessage("onPopUpEditEventSaveClicked error: {}").addArgument(e.getMessage()).log();
-
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Fehler");
             alert.setHeaderText("Ein Fehler ist aufgetreten");
             alert.setContentText("Es gab ein Problem beim speichern des Events.\n" + e.getLocalizedMessage());
             alert.showAndWait();
         }
+    }
+
+    @FXML
+    private void onCloseButtonPressed() {
+
     }
 }
